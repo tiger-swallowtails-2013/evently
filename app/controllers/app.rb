44f344d54b97bec59d4e26ae48a2985529d3a2d3
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-23 16:12:04 -0700

    user signup info saved in database

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,18 +1,25 @@
 $LOAD_PATH.unshift(File.expand_path("./app"))
 $LOAD_PATH.unshift(File.expand_path("."))
-
 require 'config/main'
-enable :sessions
 
+enable :sessions
 
 get '/' do
 	erb :index
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  User.create(params)
 end
 
 get '/login' do
   erb :login
 end
 
-post 'login' do
+post '/login' do
 
 end
