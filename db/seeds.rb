57f614bb169418836ee6d6f2c9fc3f86598ee307
Apprentice <apprentice@devbootcamp.com>
2013-09-27 12:39:06 -0700

    progress on map layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 10.times do
   user = User.create(lastname: Faker::Name.last_name, firstname: Faker::Name.first_name, email: Faker::Internet.email, password: Faker::Internet.password)
   (rand(10) + 8).times do
-    event = Event.create(title: Faker::Company.catch_phrase, description: Faker::Lorem.paragraphs(1).first, location: Faker::Address.city + ", " + Faker::Address.state, datetime: Time.now)
+    event = Event.create(title: Faker::Company.catch_phrase, description: Faker::Lorem.paragraphs(1).first, location: Faker::Address.city + ", " + Faker::Address.state, zipcode: Faker::Address.zip_code, datetime: Time.now)
     user.events << event
   end
 end
